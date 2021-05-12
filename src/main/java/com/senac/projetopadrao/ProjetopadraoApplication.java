b497780dbf154ff783c80f0a28628e17ac1dc70c
mateusco <mateusco2501@gmail.com>
2021-05-12 19:40:26 -0300

    Projeto Padrao

---
+++ src/main/java/com/senac/projetopadrao/ProjetopadraoApplication.java
@@ -0,0 +1,13 @@
+package com.senac.projetopadrao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetopadraoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetopadraoApplication.class, args);
+	}
+
+}
